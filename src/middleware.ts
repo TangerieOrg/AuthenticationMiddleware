3b20ac15e0743f53ceea0bbc846b3ce3766cf867
Tangerie <josh.cavill01@gmail.com>
2022-09-19 20:48:01 +1000

    Fixed export

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 import { getUser } from "./util";
 
-export default async function AuthMiddleware(req : Request, res : Response, next : NextFunction) {
+export async function AuthMiddleware(req : Request, res : Response, next : NextFunction) {
     req.user = await getUser(req.signedCookies["sso"])
     
     next();
